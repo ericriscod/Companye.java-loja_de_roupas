099f5129e66aa14d8b04208f808a629c123c2687
ericris.cod <rossato.ep@gmail.com>
2022-11-10 17:01:05 -0300

    StockDaoJDBC e DaoFactory

---
+++ src/br/com/modabit/model/dao/DaoFactory.java
@@ -0,0 +1,5 @@
+package br.com.modabit.model.dao;
+
+public class DaoFactory {
+
+}
---
+++ src/br/com/modabit/model/dao/Impl/StockDaoJDBC.java
@@ -0,0 +1,34 @@
+package br.com.modabit.model.dao.Impl;
+
+import java.util.List;
+
+import br.com.modabit.model.dao.StockDao;
+import br.com.modabit.model.entities.Stock;
+
+public class StockDaoJDBC implements StockDao{
+
+	@Override
+	public void insert(Stock stock) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Stock stock) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Stock> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
